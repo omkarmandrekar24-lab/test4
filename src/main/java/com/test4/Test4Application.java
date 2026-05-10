371aba9f8b0cd20514379bf8bfc3041468131ce8
omkarmandrekar24@gmail.com <omkarmandrekar24@gmail.com>
2026-05-11 00:34:37 +0530

    defectfix-3456-login-error

--- src/main/java/com/test4/Test4Application.java
+++ src/main/java/com/test4/Test4Application.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test4Application {
 
 	public static void main(String[] args) {
+
+		int x = 3300;		
+
 		SpringApplication.run(Test4Application.class, args);
 	}
 
